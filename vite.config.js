5316273c122259948af98d55bb256b52c11e1fe2
Miki-0120 <miki.tanaka07@gmail.com>
2025-05-12 22:26:31 +0900

    デプロイ用の設定

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react_todo/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    },
+    server: {
+    host: true
+    }
 })
